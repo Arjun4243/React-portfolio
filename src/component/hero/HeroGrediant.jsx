93a828565579d987b730b17240b08e8699876d5e
Arjun4243 <arjunsinghanya35@gmail.com>
2025-05-19 02:23:46 +0530

    add 4 gradient in the background

--- src/component/hero/HeroGrediant.jsx
+++ src/component/hero/HeroGrediant.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 export default function HeroGrediant() {
     return(
         <>
-        <h1>HeroGrediant</h1>
+        <div>
+            <div className='shadow-cyanMediumShadow absolute top-0 right-[400px] -z-10 animate-pulse'></div>
+
+            <div className='shadow-orangeMediumShadow absolute top-0 right-[0px] -z-10 animate-pulse'></div>
+
+            <div className='shadow-orangeMediumShadow absolute top-[300px] left-[0px] -z-10 animate-pulse'></div>
+
+            <div className='shadow-cyanMediumShadow absolute top-[500px] left-[0px] -z-10 animate-pulse'></div>
+        </div>
         </>
     )
 }
--- src/component/hero/HeroMain.jsx
+++ src/component/hero/HeroMain.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import HeroText from "./HeroText.jsx";
 import HeroPic from "./heroPic.jsx";
+import HeroGrediant from "./HeroGrediant.jsx";
 
 export default function HeroMain() {
   return (
     <>
-      <div className="here-main container w-full mx-auto flex justify-around items-center  ">
+      <div className="here-main container w-full mx-auto flex justify-around items-center sm:flex-col ">
         <HeroText></HeroText>
         <HeroPic></HeroPic>
+        <HeroGrediant></HeroGrediant>
       </div>
     </>
   );
